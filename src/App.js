2dfd3524660d25922da200c56a59402d98c5ecdd
edman <compFoxLord007@gmail.com>
2022-06-21 08:56:30 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import MenuBar from './components/MenuBar';
+import LoginForm from './components/LoginForm';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [announcements, setAnnouncements] = useState([]);
+    const [openLoginForm, setOpenLoginForm] = useState(false);
+    
+    const handleOpenLoginForm = () => setOpenLoginForm(true);
+    const handleCloseLoginForm = () => setOpenLoginForm(false);
+
+    const handleAddUser = (username) => {
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/users/add',
+            data: {
+                username: username,
+            }
+        });
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/announcements')
+            .then(res => setAnnouncements(res.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div className="App">
+            <MenuBar 
+                handleOpenLoginForm={handleOpenLoginForm}
+            />
+            <section className='announcements-list'>
+                {announcements.map(announcement => {
+                    return (
+                        <div className='card'>
+                            <img src={announcement.imageUrl} alt={announcement.title} />
+                            <h3>{announcement.title}</h3>
+                            <p>{announcement.description}</p>
+                        </div>
+                    );
+                })}
+            </section>
+            <LoginForm 
+                openLoginForm={openLoginForm}
+                handleCloseLoginForm={handleCloseLoginForm}
+                handleAddUser={handleAddUser}
+            />
+        </div>
+    );
 }
 
 export default App;
